ebfd3864ca6e274731f7de1215ebc5eeddca012c
luhf <g.luhaifeng@gmail.com>
2013-05-05 22:21:22 +0800

    Signed-off-by: luhf <g.luhaifeng@gmail.com>

--- src/main/webapp/resources/js/genCode.js
+++ src/main/webapp/resources/js/genCode.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+	alert(root + "/appFileManager/loadDir");
+	initTree(root + "/appFileManager/loadDir");//初始化tree
+});
 function init() {
 	$.ajax({
 		url : root + "/javaGenManager/getJavaBeanInfo",
@@ -6,6 +10,22 @@ function init() {
 		},
 		success : function(html) {
 			loadBeanInfo(html);
+		}
+	});
+}
+//创建目录树
+function initTree(loadNodeUrl) {
+	$('#folder').tree(
+	{
+		animate:true,
+		url : loadNodeUrl, // 'tree_data.json',
+		onClick : function(node) {
+			if (node.text.indexOf('.json') > -1) {
+				_AF.tabs.add1(node.text,root+ "/coder/genCode?filePath=" + node.id);
+			}
+		},
+		onBeforeExpand : function(node, param) {
+			$('#folder').tree('options').url = loadNodeUrl + "?node="+ node.id;
 		}
 	});
 }
--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -9,21 +9,4 @@ function init() {
 	_AF.nav.init();// 初始化导航菜单
 	_AF.tabs.init();// 绑定右键菜单事件
 	_AF.theme.init();// 初始化切换主题
-	initTree(root + "/appFileManager/loadDir");//初始化tree
-}
-// 创建目录树
-function initTree(loadNodeUrl) {
-	$('#folder').tree(
-			{
-				animate:true,
-				url : loadNodeUrl, // 'tree_data.json',
-				onClick : function(node) {
-					if (node.text.indexOf('.json') > -1) {
-						_AF.tabs.add1(node.text,root+ "/coder/genCode?filePath=" + node.id);
-					}
-				},
-				onBeforeExpand : function(node, param) {
-					$('#folder').tree('options').url = loadNodeUrl + "?node="+ node.id;
-				}
-			});
 }
