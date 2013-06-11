966f44e45cdc83e198950644455c27868488c1fd
luhf <g.luhaifeng@gmail.com>
2013-06-11 18:13:24 +0800

    修改分页bean
    Signed-off-by: luhf <g.luhaifeng@gmail.com>

--- src/main/java/com/asiainfo/appframe/common/bean/PageBean.java
+++ src/main/java/com/asiainfo/appframe/common/bean/PageBean.java
@@ -4,25 +4,47 @@ import java.io.Serializable;
 
 /**
  * 分页基类
+ * 
  * @author luhf
  * @date 2013-1-13 下午9:56:54
  */
-public class PageBean implements Serializable{
-	private static final long serialVersionUID=347630882856280040L;
-	// pagesize ，每一页显示多少
+public class PageBean implements Serializable {
+	/**
+	 * 序列化
+	 */
+	private static final long serialVersionUID = -3653593313855668121L;
+
+	/**
+	 * 每一页显示多少
+	 */
 	private int showCount;
-	// 总页数
+	/**
+	 * 总页数
+	 */
 	private int totalPage;
-	// 总记录数
+	/**
+	 * 总记录数
+	 */
 	private int totalResult;
-	// 当前页数
+	/**
+	 * 当前页数
+	 */
 	private int currentPage;
-	// 当前显示到的ID, 在mysql limit 中就是第一个参数.
+	/**
+	 * 当前显示到的ID, 在mysql limit 中就是第一个参数.
+	 */
 	private int currentResult;
+	/**
+	 * 排序字段
+	 */
 	private String sortField;
+	/**
+	 * 排序类型
+	 */
 	private String order;
 
-	public PageBean(){}
+	public PageBean() {
+	}
 
 	/**
 	 * @param rows
@@ -30,68 +52,68 @@ public class PageBean implements Serializable{
 	 * @param page
 	 *            页数
 	 */
-	public PageBean(int rows,int page){
-		if(page<=0){
-			page=1;
+	public PageBean(int rows, int page) {
+		if (page <= 0) {
+			page = 1;
 		}
-		int currentResult=(page-1)*rows;
+		int currentResult = (page - 1) * rows;
 		this.setShowCount(rows);
 		this.setCurrentResult(currentResult);
 	}
 
-	public int getShowCount(){
+	public int getShowCount() {
 		return showCount;
 	}
 
-	public void setShowCount(int showCount){
-		this.showCount=showCount;
+	public void setShowCount(int showCount) {
+		this.showCount = showCount;
 	}
 
-	public int getTotalPage(){
+	public int getTotalPage() {
 		return totalPage;
 	}
 
-	public void setTotalPage(int totalPage){
-		this.totalPage=totalPage;
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
 	}
 
-	public int getTotalResult(){
+	public int getTotalResult() {
 		return totalResult;
 	}
 
-	public void setTotalResult(int totalResult){
-		this.totalResult=totalResult;
+	public void setTotalResult(int totalResult) {
+		this.totalResult = totalResult;
 	}
 
-	public int getCurrentPage(){
+	public int getCurrentPage() {
 		return currentPage;
 	}
 
-	public void setCurrentPage(int currentPage){
-		this.currentPage=currentPage;
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
 	}
 
-	public int getCurrentResult(){
+	public int getCurrentResult() {
 		return currentResult;
 	}
 
-	public void setCurrentResult(int currentResult){
-		this.currentResult=currentResult;
+	public void setCurrentResult(int currentResult) {
+		this.currentResult = currentResult;
 	}
 
-	public String getSortField(){
+	public String getSortField() {
 		return sortField;
 	}
 
-	public void setSortField(String sortField){
-		this.sortField=sortField;
+	public void setSortField(String sortField) {
+		this.sortField = sortField;
 	}
 
-	public String getOrder(){
+	public String getOrder() {
 		return order;
 	}
 
-	public void setOrder(String order){
-		this.order=order;
+	public void setOrder(String order) {
+		this.order = order;
 	}
 }
